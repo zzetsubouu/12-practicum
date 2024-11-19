06773000ee8fd470ef97a6d7b02c82ecfd2f6a63
zzetsubouu <154281553+zzetsubouu@users.noreply.github.com>
2024-11-19 22:10:38 +0200

    Update Program.cs

--- 3task/Program.cs
+++ 3task/Program.cs
@@ -8,8 +8,24 @@ namespace _3task
 {
     internal class Program
     {
+        //Задача 3. Найти максимальную величину из двух целых переменных a, b с использованием вспомогательных методов.
         static void Main(string[] args)
         {
+            Console.Write("a=");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b=");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(Func(a, b));
+            Console.ReadKey();
+        }
+        static double Func(int a, int b)
+        {
+            //вариант с тернарной операцией
+            int result = a > b ? a : b;
+            return result;
+            
+            //2 вариант
+            //return Math.Max(a, b);
         }
     }
 }
