529f5d68ba5bd856af50d42cf6229a828d48ef50
zzetsubouu <pyanix.mladshiy@gmail.com>
2024-11-19 22:18:41 +0200

    добавлено 4 задание

---
+++ 4taskk/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4taskk
+{
+    internal class Program
+    {
+        //Задача 4. Найти максимальную величину их трех целых переменных a, b, c с использованием вспомогательных методов. 
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b=");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("c=");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine(Func(a, b, c));
+
+            Console.ReadKey();
+        }
+        static double Func(int a, int b, int c)
+        {
+            //вариант с тернарной операцией
+            int result = a > c && a > b ? a : b > c && b > a ? b : c;
+            return result;
+
+            //2 вариант
+            //return Math.Max(Math.Max(a, b), c);
+        }
+    }
+}
---
+++ 4taskk/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("4taskk")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("4taskk")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("9e94fb43-325a-407d-bfdb-6b38f6dfb75e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
