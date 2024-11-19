46619af96d512779cd3d27ffc4ca8a2580799f13
zzetsubouu <pyanix.mladshiy@gmail.com>
2024-11-19 22:04:23 +0200

    добавлено 1 задание

---
+++ 1-task/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_task
+{
+    internal class Program
+    {
+        //Задача 1. Вычислить значение выражения, определив и использовав дополнительный метод:
+
+        static void Main(string[] args)
+        {
+            double y = Func(6) + Func(13) + Func(21);
+            Console.WriteLine($"y={y}");
+            Console.ReadKey();
+        }
+        static double Func(int x)
+        {
+            return (Math.Sqrt(x) + x) / 2;
+        }
+    }
+}
---
+++ 1-task/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("1-task")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("1-task")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("59bb7b25-351c-43e3-9438-b6e4a211e338")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
